581f4b5eeed205d9ee9a11c6d64b318a51f289a6
Sudharshan 'Sup3rkiddo' S <sudharsh@gmail.com>
2011-02-28 22:25:19 +0530

    Release 0.0.1 :P

--- static/js/ragesquared.js
+++ static/js/ragesquared.js
@@ -1,31 +1,68 @@
 var score = 0;
-var time = 59;
+var time = 0;
+var timeouts = new Array();
+var timerId = 0;
+
+function createSquare(paper, side, points, timeout) {
+    coords = getRandomCoords();
+    rect = paper.rect(coords['x'], coords['y'], side, side);
+    rect.click(function() {
+        score = score + points;
+        $("#score").fadeIn('fast', function() { $("#score").html(score); });
+        
+        refreshPosition(rect, timeout);
+    });
+    var intervalId = window.setInterval(refreshPosition, timeout + 200, rect);
+    return [rect, intervalId];
+}
 
 function startGame(paper) {
-    coords = getRandomCoords();
-    rect = paper.rect(coords['x'], coords['y'], 75, 75);
-    rect.attr("fill", "#f00");
-    rect.click(function() {
-        score = score + 10;
-        $("#score").html(score);
-        refreshPosition(rect);
-    });
-    var intervalId = window.setInterval(refreshPosition, 1000, rect);
-    var timerId = window.setInterval(refreshTimer, 1000);
-    var timeOut = window.setTimeout(stopGame, 60000, rect, intervalId, timerId)
+    // Slow rectangle
+    var rect1 = createSquare(paper, 75, 10, 500);
+    rect1[0].attr("fill", "#f00");
+    timeouts.push(rect1[1]);
+    
+    var rect2 = createSquare(paper, 60, 20, 400);
+    rect2[0].attr("fill", "#6b0fe7");
+    timeouts.push(rect2[1]);
+
+    var troll_coords = getRandomCoords();
+    troll_face = paper.image("static/trollface.svg", troll_coords['x'], troll_coords['y'], 75, 75);
+    
+    timerId = window.setInterval(refreshTimer, 1000, paper);
 }
 
 
-function stopGame(rect, intervalId, timerId) {
-    clearInterval(timerId);
-    clearInterval(intervalId);
-    rect.remove();
+function stopGame(paper, timeouts) {
+    for (_id in timeouts)
+        clearTimeout(_id);
+    paper.clear();
+    $("#game").fadeOut('fast',
+                           function() {
+                               $("#final_score").html("Your Score: " + score);
+                               $("#play_again").click(
+                                   function() {
+                                       $("#summary").fadeOut('fast',
+                                                             function() {
+                                                                 $("#game").fadeIn(
+                                                                     'fast', initialize
+                                                                 );
+                                                             });
+                                       });
+                                                           
+                               $("#summary").fadeIn('fast');
+                           });
 }
     
 
-function refreshTimer() {
+function refreshTimer(paper) {
     time = time - 1;
     $("#timer").html(time);
+
+    if (time == 0) {
+        clearInterval(timerId);
+        stopGame(paper, timeouts);
+    }    
 }
 
 
@@ -36,40 +73,48 @@ function getRandomCoords() {
     var y1 = parseInt($("#stage").css("margin-top").slice(0, -2));
     var y2 = parseInt($("#stage").css("margin-top").slice(0, -2)) + $("#stage").height();
     
-    var x = Math.round(Math.random() * 1000);
-    var y = Math.round(Math.random() * 1000);
-
-    var _x = x; var _y = y;
-    if (x < x1)
-        x = x + x1;
-    else if (x > x2)
-        x = x - x2;
-
-    if (y > y2)
-        y = y - y2;
-    else if (y < y1)
-        y = y + y1;
+    var x = Math.floor(Math.random() * (x2 - x1 + 1)) + x1;
+    var y = Math.floor(Math.random() * (y2 - y1 + 1)) + y1;
 
     return {'x': x, 'y': y};
 }
 
 
-function refreshPosition(rect) {
+function refreshPosition(rect, timeout) {
     new_coords = getRandomCoords();
 
-    rect.animate({scale: 0}, 200,
+    rect.animate({scale: 0}, 300,
                  function() {
-                     this.attr({"x": new_coords['x'],
+                     rect.attr({"x": new_coords['x'],
                                 "y": new_coords['y']
                                });
-                     this.animate({scale: 1}, 500);        
+                     rect.animate({scale: 1}, timeout);  
                  });
 }
 
-
-$(function() {
+initialize = function() {
     width = $("#stage").width();
     height = $("#stage").height();
     var paper = Raphael("stage", width, height);
+    score = 0;
+    time = 30;
     startGame(paper);
+}
+                               
+
+$(function() {
+    $("#game").hide();
+    $("#summary").hide();
+    $("#start").fadeTo(0.2);
+      
+
+    $("#start").click(
+        function() {
+            $("#welcome").fadeOut('fast',
+                                  function() {
+                                      $("#game").fadeIn('fast',
+                                                        initialize
+                                                        );
+                                      })
+            });
 });
